9303e7ba7ec5365c56dd96c5ae2e02883292ec12
vladimir <vladimirmuratov@mail.ru>
2024-09-05 14:44:33 +0300

    fix margin top

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -22,78 +22,24 @@ export const Banner = () => {
     }, [])
 
     return (
-        /*<Box
-            component="figure"
-            className="banner-container"
-            sx={{
-                position: 'relative',
-            }}
-        >
-            <img className="banner" src="/images/images-for-landings/3.webp" alt="banner"/>
-            <Box sx={{
-                position: 'absolute',
-                top: {xs: '10%', sm: '20%'},
-                left: '5%',
-            }}>
-                <Typography
-                    variant="h4"
-                    sx={{
-                        fontSize: {xs: 32, sm: 58, md: 62},
-                        lineHeight: 1.1,
-                        fontWeight: 600,
-                        color: 'var(--black)',
-                        textShadow: '0px 4px 4px lightgray',
-                        marginBottom: {xs: '5px', sm: '10px'}
-                    }}>
-                    Работа<br/>
-                    с тяжелыми<br/>
-                    пациентами
-                </Typography>
-
-                {isMobile
-                    ? (<Box
-                    sx={{
-                        display: 'inline'
-                    }}
-                    >
-                        <Button
-                            onClick={() => router.push(`tel:${phoneNumber}`)}
-                            variant="contained"
-                            color="error"
-                            size="large"
-                            sx={{
-                                display: isMobile ? 'block' : 'none'
-                            }}
-                        >
-                            Позвонить
-                        </Button>
-                        <SocialBlock className='banner-social-mobile'/>
-                    </Box>)
-                    : <Typography
-                        sx={{
-                            color: 'var(--red)',
-                            display: isMobile ? 'none' : 'block',
-                            fontSize: {xs: 16, sm: 32, md: 62},
-                            fontWeight: 600,
-                            textShadow: '0px 4px 4px lightgray',
-                        }}>{phoneNumber}</Typography>
-                }
-            </Box>
-        </Box>*/
-
         <Box
             component="figure"
             className="banner-container"
             sx={{
-                position: 'relative'
+                position: 'relative',
+                marginTop: isMobile ? {xs: '110px', sm: '95px'} : 0,
             }}
         >
 
             <Box
                 sx={{
                     width: '100%',
-                    // height: {xs: '129px', sm: '241px', md: '322px', lg: '459px'},
-                    height: {xs: 'calc(var(--index) * 19)', sm: 'calc(var(--index) * 26)', md: 'calc(var(--index) * 33)', lg: 'calc(var(--index) * 33)'},
+                    height: {
+                        xs: 'calc(var(--index) * 19)',
+                        sm: 'calc(var(--index) * 26)',
+                        md: 'calc(var(--index) * 33)',
+                        lg: 'calc(var(--index) * 33)'
+                    },
                     backgroundImage: `url(/images/images-for-landings/3.webp)`,
                     backgroundSize: 'cover',
                 }}
