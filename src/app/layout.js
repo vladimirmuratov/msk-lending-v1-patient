7c742051d506fe89b32ccbc00f21e350c4d70251
vladimir <vladimirmuratov@mail.ru>
2024-12-15 18:02:26 +0300

    update layout metadata and Metrika

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,29 @@
-import {AppRouterCacheProvider} from '@mui/material-nextjs/v14-appRouter'
-import {ThemeProvider} from '@mui/material/styles'
-import {Montserrat} from 'next/font/google'
-import {Box} from '@mui/material'
-import {Footer} from '@/components/Footer'
-import {Navigation} from '@/components/navigation/Navigation'
-// import {Metrika} from '@/components/Metrika'
-import {Suspense} from 'react'
-import theme from '@/theme'
-import './globals.css'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import { Montserrat } from 'next/font/google';
+import { Box } from '@mui/material';
+import { Footer } from '@/components/Footer';
+import { Navigation } from '@/components/navigation/Navigation';
+import {Metrika} from '@/components/Metrika'
+import { Suspense } from 'react';
+import theme from '@/theme';
+import './globals.css';
 
-const montserrat = Montserrat({subsets: ['latin', 'cyrillic']})
+const montserrat = Montserrat({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata = {
-    title: 'МСК-Групп',
-    description: 'Платная госпитализация в федеральные лечебные учреждения Москвы. Уход за тяжелобольными пациентами',
-    keywords: 'платная госпитализация в москве, скорая помощь, уход за тяжелобольными пациентами'
-}
+    title: 'Лечение тяжелых пациентов. Госпитализация +7 (499) 719-81-00, 24/7',
+    description: 'Качественное лечение тяжелых пациентов. Госпитализация 24/7. Позвоните по номеру +7 (499) 719-81-00 для консультации и помощи.',
+    keywords: ['тяжелый случай пациент', 'пациент находится в тяжелом состоянии', 'уход за тяжелым пациентом', 'скорая помощь тяжелый пациент'],
+    other: {
+        ['yandex-verification']: '8a67daf7ca228bdf',
+    },
+    alternates: {
+        canonical: 'https://patient.mskdoctor.ru/'
+    }
+};
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
     return (
         <html lang="en">
         <body className={montserrat.className}>
@@ -25,15 +31,15 @@ export default function RootLayout({children}) {
             <ThemeProvider theme={theme}>
                 <Box className="container">
                     <Suspense>
-                        <Navigation/>
+                        <Navigation />
                         {children}
-                        {/*<Metrika/>*/}
-                        <Footer/>
+                        <Footer />
+                        <Metrika/>
                     </Suspense>
                 </Box>
             </ThemeProvider>
         </AppRouterCacheProvider>
         </body>
         </html>
-    )
+    );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -14,49 +14,30 @@ import { FormAction } from '@/components/FormAction';
 import { MainInfoBlock } from '@/components/MainInfoBlock';
 import { CoordinatorBlock } from '@/components/CoordinatorBlock';
 import PhoneBlock from '@/components/PhoneBlock';
-import { Suspense } from 'react';
-import { Metrika } from '@/components/Metrika';
-
-export const metadata = {
-    title: 'Лечение тяжелых пациентов. Госпитализация +7 (499) 719-81-00, 24/7',
-    description: 'Качественное лечение тяжелых пациентов. Госпитализация 24/7. Позвоните по номеру +7 (499) 719-81-00 для консультации и помощи.',
-    keywords: ['тяжелый случай пациент', 'пациент находится в тяжелом состоянии', 'уход за тяжелым пациентом', 'скорая помощь тяжелый пациент'],
-    other: {
-        ['yandex-verification']: '8a67daf7ca228bdf',
-    },
-    alternates: {
-        canonical: 'https://patient.mskdoctor.ru/'
-    }
-};
 
 export default function Home() {
     return (
-        <>
-            <main style={{ position: 'relative' }}>
-                <Banner />
-                <CardsBlock />
-                <PhoneBlock />
-                <MainInfoBlock />
-                <PhoneBlock />
-                <AboutBlock />
-                <WhyWeBlock />
-                <PhoneBlock />
-                <WeRespondBlock />
-                {/*<WhatWeOfferBlock/>*/}
-                <DirectionsOfHospitalizationBlock />
-                <PhoneBlock />
-                <BaseAccordion id="faq" title="Частые вопросы" items={faq} />
-                <CoordinatorBlock />
-                <PartnersBlock />
-                <PhoneBlock />
-                <BaseAccordion id="service" title="Услуги" items={services} />
-                <TextBlock text={text1} />
-                <ContactsBlock />
-                <FormAction />
-            </main>
-            <Suspense>
-                <Metrika />
-            </Suspense>
-        </>
+        <main style={{ position: 'relative' }}>
+            <Banner />
+            <CardsBlock />
+            <PhoneBlock />
+            <MainInfoBlock />
+            <PhoneBlock />
+            <AboutBlock />
+            <WhyWeBlock />
+            <PhoneBlock />
+            <WeRespondBlock />
+            {/*<WhatWeOfferBlock/>*/}
+            <DirectionsOfHospitalizationBlock />
+            <PhoneBlock />
+            <BaseAccordion id="faq" title="Частые вопросы" items={faq} />
+            <CoordinatorBlock />
+            <PartnersBlock />
+            <PhoneBlock />
+            <BaseAccordion id="service" title="Услуги" items={services} />
+            <TextBlock text={text1} />
+            <ContactsBlock />
+            <FormAction />
+        </main>
     );
 }
